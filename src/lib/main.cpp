4b8843c0a8478a30fcef96f71f585b20ebd635f6
NileshMagan <nilesh.magan@gmail.com>
2018-07-25 13:42:02 +1200

    Main has layen has layers linked without for loops

--- src/lib/main.cpp
+++ src/lib/main.cpp
@@ -16,9 +16,20 @@ void neural_net(MEAN_IMAGE_TYPE mean_image[nChannels][imgHeight][imgWidth], INPU
 				float temp =  (input_image[i][j][k] - mean_image[i][j][k]) * dataScale;
 				*GET_INPUT_DATA(layers[0], i,j,k) = temp;
 			}
-	for(int i = 0;i<nLayers;i++){
-		layer_dict[layers[i].type](layers[i], layers[i+1]);
-	}
+	// for(int i = 0;i<nLayers;i++){
+	// 	layer_dict[layers[i].type](layers[i], layers[i+1]);
+	// }
+
+	Convolution(layers[0], layers[1]);
+	PoolingMax(layers[1], layers[2]);
+	Convolution2(layers[2], layers[3]);
+	PoolingMax(layers[3], layers[4]);
+	InnerProduct(layers[4], layers[5]);
+	Relu(layers[5], layers[6]);
+	InnerProduct2(layers[6], layers[7]);
+	Softmax(layers[7], layers[8]);
+	ReturnCallback(layers[8], layers[9]);
+
 
 	for(int i = 0;i<nOutput;i++){
 		result[i] = *(layers[nLayers-1].input_data+i);
